3da7fc127cd9e8fe044ec97067a2490741ae7632
Rishabh TNT <rishabh221@gmail.com>
2026-02-01 23:27:29 +0530

    pass generator complete

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
+
 function App() {
   const [length, setLength] = useState(8);
+  const [useNum, setUseNum] = useState(false);
+  const [useChar, setUseChar] = useState(false);
+
+  const [password, setPassword] = useState(generateCode);
+
+  const generatePassword = useCallback(generateCode, [length, useNum, useChar]);
+
+  const passRef = useRef(null);
+
+  useEffect(() => {
+    const newPassword = generatePassword();
+    setPassword(newPassword);
+  }, [generatePassword]);
 
   return (
     <>
       <div className="bg-gray-600 p-4 flex flex-col gap-2">
         <div style={{ display: "flex" }}>
-          <input type="text" style={{ backgroundColor: "white", flex: 1 }} />
+          <input
+            type="text"
+            style={{ backgroundColor: "white", flex: 1 }}
+            value={password}
+            ref={passRef}
+            readOnly
+          />
           <button
             style={{
               backgroundColor: "rgb(0, 120, 212)",
               color: "white",
               fontWeight: "bold",
             }}
+            onClick={copytoClipboard}
           >
             Copy
           </button>
@@ -27,31 +48,57 @@ function App() {
             color: "rgb(82, 146, 215)",
           }}
         >
-          <input type="range" name="length" id="range" onInput={generateCode} />
-          <label htmlFor="range">Length</label>
-          <input type="checkbox" name="numberAllowed" id="number" />
+          <input
+            type="range"
+            name="length"
+            id="range"
+            onChange={sliderHandler}
+          />
+          <label htmlFor="range">Length ({length})</label>
+          <input
+            type="checkbox"
+            name="numberAllowed"
+            id="number"
+            onChange={() => {
+              setUseNum(!useNum);
+            }}
+          />
           <label htmlFor="number">Include Numbers</label>
-          <input type="checkbox" name="charactersAllowed" id="character" />
+          <input
+            type="checkbox"
+            name="charactersAllowed"
+            id="character"
+            onChange={() => {
+              setUseChar(!useChar);
+            }}
+          />
           <label htmlFor="character">Include Special Charactres</label>
         </div>
       </div>
     </>
   );
-  function generateCode() {
-    console.log("length: ", length);
-    const input = document.querySelector('input[type="text"]');
-    // const alpha = String.fromCharCode(Math.floor(Math.random() * 58 + 65));
-    const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    const alphanum = alpha + "0123456789";
-    let code = "";
-    const useNum = true;
 
-    for (let i = 0; i <= length; i++) {
-      code += useNum
-        ? alphanum[Math.floor(Math.random() * alphanum.length + 1)]
-        : alpha[Math.floor(Math.random() * alpha.length + 1)];
+  function generateCode() {
+    let code = "";
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (useNum) chars += "0123456789";
+    if (useChar) chars += "!@#$%^&*";
+
+    for (let i = 1; i <= length; i++) {
+      code += chars[Math.floor(Math.random() * chars.length)];
     }
-    input.value = code;
+    return code;
+  }
+
+  function sliderHandler(e) {
+    const newLen = e.target.value;
+    console.log(newLen);
+    setLength(newLen);
+  }
+
+  function copytoClipboard() {
+    passRef.current?.select();
+    window.navigator.clipboard.writeText(password);
   }
 }
 
